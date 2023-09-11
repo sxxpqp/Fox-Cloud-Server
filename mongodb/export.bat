fdacb6986f96cb66f0eeeeb85cfb6a3c06254e6d
hupengwu <5389408@qq.com>
2023-09-11 19:33:05 +0800

    mongodb的刷庫脚本

---
+++ mongodb/export.bat
@@ -0,0 +1,1 @@
+mongoexport -h 39.108.137.38 -u admin -p 12345678 -d fox-cloud-server-aggregator -c edgeEntitySchema -o ./edgeEntitySchema.json
---
+++ mongodb/import.bat
@@ -0,0 +1,2 @@
+mongoimport -h 39.108.137.38 -u admin -p 12345678  --db fox-cloud-server-aggregator -c edgeEntitySchema  --type json ./edgeEntitySchema.json
+
